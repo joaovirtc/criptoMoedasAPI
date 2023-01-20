4f2fd149d097eb584027eed9312f0b25beb9f27f
JoaoVictor <joaovictorcarmindo@gmail.com>
2023-01-20 15:37:10 -0300

    projeto pronto

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+import axios from "axios";
+import Coin from "./components/Coin";
+import "remixicon/fonts/remixicon.css";
+import React from "react";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Pesquise Crypto Moedas</h1>
+        <form>
+          <div className="inputContainer">
+            <i className="ri-search-2-line"></i>
+            <input
+              className="coin-input"
+              type="text"
+              onChange={handleChange}
+              placeholder="Pesquisar"
+            />
+          </div>
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
